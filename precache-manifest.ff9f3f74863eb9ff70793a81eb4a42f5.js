f66b7ee062cc341d67e8437383b151c0e61502b1
WOLFCLAW <frostwolfclaw@gmail.com>
2018-12-20 11:13:20 +0900

    Updates

---
+++ precache-manifest.ff9f3f74863eb9ff70793a81eb4a42f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac5f16a0642b1e384a40",
+    "url": "/react-lingui-demo/static/js/runtime~main.ac5f16a0.js"
+  },
+  {
+    "revision": "7c4b59bf92ee939500ff",
+    "url": "/react-lingui-demo/static/js/main.7c4b59bf.chunk.js"
+  },
+  {
+    "revision": "fc4e930c3109bea342dd",
+    "url": "/react-lingui-demo/static/js/i18n-2.fc4e930c.chunk.js"
+  },
+  {
+    "revision": "117e1452b55ab1e17ed3",
+    "url": "/react-lingui-demo/static/js/i18n-1.117e1452.chunk.js"
+  },
+  {
+    "revision": "2481d6903700e52f01e5",
+    "url": "/react-lingui-demo/static/js/i18n-0.2481d690.chunk.js"
+  },
+  {
+    "revision": "7578e8c1e3ddac35fb64",
+    "url": "/react-lingui-demo/static/js/4.7578e8c1.chunk.js"
+  },
+  {
+    "revision": "7c4b59bf92ee939500ff",
+    "url": "/react-lingui-demo/static/css/main.d76a285c.chunk.css"
+  },
+  {
+    "revision": "914179be0560a92d730950fa66436a38",
+    "url": "/react-lingui-demo/index.html"
+  }
+];
