ac12b7828a030cf398a6615fa8dd759b23daee8d
Nukbal <wolfclaw@Nukbals-MacBook-Pro.local>
2019-05-21 21:35:57 +0900

    Updates

---
+++ precache-manifest.45818e1822ef8c79d0434547ffc85ef8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7f34296fe0ed88283014da29381823",
+    "url": "/react-lingui-demo/index.html"
+  },
+  {
+    "revision": "f01086c527f8c870387b",
+    "url": "/react-lingui-demo/static/css/main.36883630.chunk.css"
+  },
+  {
+    "revision": "7f25fd2e14527c695652",
+    "url": "/react-lingui-demo/static/js/5.11c5ba49.chunk.js"
+  },
+  {
+    "revision": "51c18c0eda9a362db5cf",
+    "url": "/react-lingui-demo/static/js/i18n-0.44e71000.chunk.js"
+  },
+  {
+    "revision": "55c163f7ab9d5f85b7c4",
+    "url": "/react-lingui-demo/static/js/i18n-1.7450c420.chunk.js"
+  },
+  {
+    "revision": "7393c33ce83d898a2a24",
+    "url": "/react-lingui-demo/static/js/i18n-2.6c13f8b1.chunk.js"
+  },
+  {
+    "revision": "f01086c527f8c870387b",
+    "url": "/react-lingui-demo/static/js/main.7f79dcc3.chunk.js"
+  },
+  {
+    "revision": "25262cca705af36a8b50",
+    "url": "/react-lingui-demo/static/js/runtime~main.2734a427.js"
+  }
+]);
